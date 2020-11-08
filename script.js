53e3d2dfbc9bb73386778b908368644d37654bcd
Zoltan Horvath <zoltan.horvath1990@gmail.com>
2020-11-08 16:48:02 +0100

    add final form of navigation layout

---
+++ script.js
@@ -0,0 +1,10 @@
+
+/* Basic functionality of navbar hamburger menu */
+
+const menuIcon = document.querySelector('.burger');
+const navbar = document.querySelector('.nav-list');
+
+menuIcon.addEventListener('click', () => {
+  navbar.classList.toggle('change');
+});
+
