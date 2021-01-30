eef09f6566b34b5e41285589bca773c47cf764b9
Zoltan Horvath <zoltan.horvath1990@gmail.com>
2021-01-30 21:18:50 +0100

    rewrite most of the page's layout

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 
 // Basic functionality of navbar hamburger menu 
 
-const menuIcon = document.querySelector('.burger');
-const navbar = document.querySelector('.nav-list');
+const menuIcon = document.querySelector('.hamburger');
+const navbar = document.querySelector('.nav-links');
+const menuItems = document.querySelectorAll('.nav-link');
 
 menuIcon.addEventListener('click', () => {
   navbar.classList.toggle('change');
+});
+menuItems.forEach((item) => {
+  item.addEventListener('click', () => {
+    navbar.classList.remove('change');
+  });
 });
 
 // Scrolling on the page with smooth scroll
